89f3febf487598ce7c2bfc2202be6bd86b81b886
Antipkin-A <Artem.Antipkin@onlyoffice.com>
2023-02-08 17:47:00 +0300

    jwt header from config

--- download.php
+++ download.php
@@ -33,7 +33,8 @@ global $DB;
 
 $modconfig = get_config('onlyofficeeditor');
 if (!empty($modconfig->documentserversecret)) {
-    $token = substr(getallheaders()['Authorization'], strlen('Bearer '));
+    $jwtheader = !empty($modconfig->jwtheader) ? $modconfig->jwtheader : 'Authorization';
+    $token = substr(getallheaders()[$jwtheader], strlen('Bearer '));
     try {
         $decodedheader = \Firebase\JWT\JWT::decode($token, $modconfig->documentserversecret, array('HS256'));
     } catch (\UnexpectedValueException $e) {
