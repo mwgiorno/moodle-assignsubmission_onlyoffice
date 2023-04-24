eade3d79a9b47bd037ba550441cfa51310d4557f
Sergey Linnik <sergey.linnik@onlyoffice.com>
2023-04-24 14:30:28 +0500

    update version

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'assignsubmission_onlyoffice';
-$plugin->version = 2023031800;
+$plugin->version = 2023042400;
 $plugin->release = 'v1.0.0';
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->dependencies = array('mod_onlyofficeeditor' => 2023032100);
+$plugin->dependencies = array('mod_onlyofficeeditor' => 2023042400);
