6c8fa7887324615a27cd991bbd87c32a9b781d66
Serik Ibragimov <serik.kerimbayevich@onlyoffice.com>
2024-06-26 14:24:23 +0500

    get initial file from templates area for pdf format

--- locallib.php
+++ locallib.php
@@ -159,6 +159,8 @@ class assign_submission_onlyoffice extends assign_submission_plugin {
 
         $initialfile = null;
         if ($cfg->format === 'pdf') {
+            $initialfile = filemanager::get_template($contextid);
+        } else if ($cfg->format === 'docxf') {
             $initialfile = filemanager::get_initial($contextid);
         }
 
