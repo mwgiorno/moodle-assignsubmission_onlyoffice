2cd790687a677f3783e2cfa92c6b6269867453c2
Serik Ibragimov <serik.kerimbayevich@onlyoffice.com>
2025-05-22 15:48:27 +0500

    update minimum moodle requirement to v4.2.0

--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'assignsubmission_onlyoffice';
 $plugin->version = 2025052100;
 $plugin->release = 'v3.1.0';
-$plugin->requires = 2022041900;
+$plugin->requires = 2023042400;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = ['mod_onlyofficeeditor' => 2024111300];
